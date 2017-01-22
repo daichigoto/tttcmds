f6e30a2e15f6bb378fee2eeecbf1b5d91c6b8fd2
Daichi GOTO <daichigoto@gmail.com>
2017-01-22 17:58:39 +0900

    Added a new command retu_select_max that outputs a max value of the specified column.

---
+++ src/bin/retu_select_max/command.h
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2016,2017 Daichi GOTO
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are 
+ * met: 
+ * 
+ * 1. Redistributions of source code must retain the above copyright 
+ *    notice, this list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in the 
+ *    documentation and/or other materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#define VERSION "20170122"
+#define CMDNAME "retu_select_max"
+#define ALIAS "retusel_max col_select_max"
+
+#include "ttt.h"
+
+#define TGT_GYO_PROCESS(GYO_BUFFER,NF) \
+	if (FLAG_1 && first_line) \
+		first_line = 0; \
+	else { \
+		for (int i = 1; i <= R_ARGC; i++) \
+			if (R_ARGV[i] <= NF && \
+				NULL != GYO_BUFFER[R_ARGV[i]]) { \
+				MAX(GYO_BUFFER[R_ARGV[i]],i); \
+			} \
+	}
+
+#define MAX(TARGET,I) { \
+	if ('\0' != TARGET[0] && '@' != TARGET[0] && '\0' != TARGET[1]) { \
+		if (0 == strcmp("@", max[I])) { \
+			if (FLAG_N) { \
+				num = (int)strtol(TARGET, \
+					(char **)NULL, 10); \
+				if (EINVAL != errno) { \
+					strcpy(max[I], \
+						_str2ssvstr(TARGET)); \
+					nummax[I] = num; \
+				} \
+			} \
+			else \
+				strcpy(max[I], _str2ssvstr(TARGET)); \
+		} \
+		else { \
+			if (FLAG_N) { \
+				num = (int)strtol(TARGET, \
+					(char **)NULL, 10); \
+				if (EINVAL != errno) { \
+					if (num > nummax[I]) { \
+						strcpy(max[I], \
+						_str2ssvstr(TARGET)); \
+						nummax[I] = num; \
+					} \
+				} \
+			} \
+			else { \
+				if (strcmp(TARGET, max[I]) > 0) \
+					strcpy(max[I], \
+						_str2ssvstr(TARGET)); \
+			} \
+		} \
+	} \
+}
---
+++ src/bin/retu_select_max/retu_select_max.c
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2016,2017 Daichi GOTO
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are 
+ * met: 
+ * 
+ * 1. Redistributions of source code must retain the above copyright 
+ *    notice, this list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in the 
+ *    documentation and/or other materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "command.h"
+
+int
+main(int argc, char *argv[])
+{
+	getcmdargs(argc, argv, "1d:NhvD",
+	           CMDARGS_R_NEED|CMDARGS_R_ARGARG_NONE);
+
+	char max[R_ARGC+1][4096];
+	int nummax[R_ARGC+1];
+
+	int num;
+	for (int i = 1; i <= R_ARGC; i++) {
+		max[i][0] = '@';
+		max[i][1] = '\0';
+	}
+	int first_line = 1;
+
+	FILEPROCESS_GYO
+
+#define PRINT_VALUE(I,DELIMITER) \
+	if (FLAG_d) { \
+		if ('@' == max[I][0] && '\0' == max[I][1]) \
+			printf("%s%s", FLAG_d_ARG, DELIMITER); \
+		else \
+			printf("@%s", DELIMITER); \
+	} else \
+		printf("%s%s", max[I], DELIMITER);
+
+	for (int i = 1; i < R_ARGC; i++) {
+		PRINT_VALUE(i, " ")
+	}
+	PRINT_VALUE(R_ARGC, "\n")
+
+	exit(EX_OK);
+}
