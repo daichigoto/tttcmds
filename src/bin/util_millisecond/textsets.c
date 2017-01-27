d073b628590376887589296ce0af42e5db2bc052
Daichi GOTO <daichigoto@gmail.com>
2017-01-27 16:09:15 +0900

    Added a new command milliseecond that display the current date and time in milliseconds.

---
+++ src/bin/util_millisecond/textsets.c
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2017 Daichi GOTO
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are 
+ * met: 
+ * 
+ * 1. Redistributions of source code must retain the above copyright 
+ *    notice, this list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in the 
+ *    documentation and/or other materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "command.h"
+
+struct textset cmdtextsets[] = {
+	{ "command_version", "en_", VERSION },
+
+	{ "command_name", "en_", CMDNAME },
+
+	{ "command_alias", "en_", ALIAS },
+
+	{ "command_comment", "ja_JP", "現在時刻をミリ秒で表示" },
+
+	{ "command_copyright", "en_", "2017 ONGS Inc." },
+
+	{ "command_comment", "en_", 
+	  "display current date and time in milliseconds" },
+
+	{ "command_synopsis", "en_",
+	  _CMD(CMDNAME) " "
+	  "[" _OPT("hvD") "] [" _OPT("-") "] " },
+
+	{ "command_description", "ja_JP", "現在時刻をミリ秒で表示。" },
+
+	{ "command_description", "en_", 
+	  "Display the current date and time in milliseconds." },
+
+	{ "command_options", "ja_JP", 
+	  _OPT("h") "		使い方表示\n"
+	  _OPT("v") "		バージョン表示\n"
+	  _OPT("D") "		デバッグモード\n"
+	  _OPT("-") "		オプションの終了を指定" },
+	
+	{ "command_options", "en_", 
+	  _OPT("h") "		Print the usage message.\n"
+	  _OPT("v") "		Print the version.\n"
+	  _OPT("D") "		Enable the debug mode.\n"
+	  _OPT("-") "		Specify the end of options." },
+	
+	{ "command_example", "en_",
+	  _P("util_millisecond")
+	  _S("1485498984677")
+	  _P("util_millisecond")
+	  _S("1485499033709")
+	  _P("") },
+
+	TEXTSET_END
+};
