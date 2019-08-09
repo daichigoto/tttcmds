b1e5ec2f86751e2c049642583ed20c80985f693a
Daichi GOTO <daichigoto@gmail.com>
2019-08-09 11:36:25 +0900

    added a new command conv_ssv2yukossv

---
+++ src/bin/conv_ssv2yukossv/conv_ssv2yukossv.c
@@ -0,0 +1,194 @@
+/*
+ * Copyright (c) 2019 Daichi GOTO
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are 
+ * met: 
+ * 
+ * 1. Redistributions of source code must retain the above copyright 
+ *    notice, this list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in the 
+ *    documentation and/or other materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "command.h"
+
+static inline void print_line(char *, int);
+
+int
+main(int argc, char *argv[])
+{
+	getcmdargs(argc, argv, "dhvD", CMDARGS_R_NONE);
+
+	DB	*btree;
+	DBT	key, val;
+
+	FILE	*fp;
+	char	line_buf[LINE_BUF_MAX];
+	int	line_len;
+
+	char	id_buf[LINE_BUF_MAX];
+	int	id_len;
+
+	// Create btree storage
+	btree = dbopen(NULL, O_RDWR, 0, DB_BTREE, NULL);
+
+	fp = fopen(F_ARGV[1], "r");
+	if (NULL == fp)
+		err(errno, "%s", F_ARGV[1]);
+
+	while (NULL != fgets(line_buf, LINE_BUF_MAX, fp)) {
+		// Store the value of the first and the second 
+		// column as ID
+		char *p;
+		p = line_buf;
+		id_len = 0;
+		while (IS_NOT_SEPARATOR(p)) {
+			++id_len;
+			++p;
+		}
+		if (' ' == *p) {
+			++id_len;
+			++p;
+		}
+		while (IS_NOT_SEPARATOR(p)) {
+			++id_len;
+			++p;
+		}
+		memcpy(id_buf, line_buf, id_len * sizeof(char));
+		*(id_buf + id_len) = '\0';
+
+		/*
+		 * Remove the line feed from the line
+		 */
+		char *p_to_linefeed;
+		line_len = strlen(line_buf);
+		p_to_linefeed = line_buf + line_len - 1;
+		if ('\n' == *p_to_linefeed)
+			*p_to_linefeed = '\0';
+
+		/*
+		 * Store the row in btree with ID as an uniq key.
+		 * The same ID's data will be overwritten.
+		 */
+		key.data = id_buf;
+		key.size = id_len + 1;
+		val.data = line_buf;
+		val.size = line_len + 1;
+
+		btree->put(btree, &key, &val, 0);
+	}
+	fclose(fp);
+
+	/*
+	 * For each ID, output the most recent and valid data.
+	 * The valid in this case means that the value in the fourth 
+	 * column is "有効" in UTF-8.
+	 * 	UTF-8:
+	 * 		有効 == 0xE6 0x9C 0x89
+	 * 		無効 == 0xE7 0x84 0xA1
+	 */
+	char *previous_line;
+	int key_len;
+
+	key_len = 0;
+	previous_line = NULL;
+	while (0 == btree->seq(btree, &key, &val, R_NEXT)) {
+		char *p;
+		p = val.data;
+
+		// BEGIN
+		if (NULL == previous_line) {
+			previous_line = val.data;
+			while (IS_NOT_SEPARATOR(p)) {
+				++p;
+			}
+			key_len = p - previous_line;
+			continue;
+		}
+		// ID is the same as previous line	
+		else if (0 == strncmp(previous_line, val.data, key_len)) {
+			previous_line = val.data;
+			continue;
+		}
+		// ID is different from the previous line.
+		// Check the 4th column and print if valid.
+		else {
+			print_line(previous_line, FLAG_d);
+			previous_line = val.data;
+		}
+	}
+	// END
+	print_line(previous_line, FLAG_d);
+
+	exit(EX_OK);
+}
+
+static inline void
+print_line(char *line, int delete_flag)
+{
+	if (NULL == line)
+		return;
+
+	char *p;
+	p = line;
+
+	// Move the pointer to the fourth row
+	while (IS_NOT_SEPARATOR(p))
+		++p;
+	if (' ' == *p) 
+		++p;
+	while (IS_NOT_SEPARATOR(p))
+		++p;
+	if (' ' == *p) 
+		++p;
+	while (IS_NOT_SEPARATOR(p))
+		++p;
+
+	// Do not output because of inappropriate format
+	if (' ' != *p) 
+		return;
+
+	++p;
+	IS_YUKO(p) {
+		p = line;
+		if (delete_flag) {
+			// Output the first column
+			while (' ' != *p) {
+				putchar(*p);
+				++p;
+			}
+			// Skip the send column
+			++p;
+			while (' ' != *p)
+				++p;
+			// Skip the third column
+			++p;
+			while (' ' != *p)
+				++p;
+			// Skip the fourth column
+			++p;
+			while (IS_NOT_SEPARATOR(p)) 
+				++p;
+			printf("%s\n", p);
+
+		}
+		else {
+			printf("%s\n", p);
+		}
+	}
+}
---
+++ src/bin/conv_ssv2yukossv/textsets.c
@@ -0,0 +1,156 @@
+/*
+ * Copyright (c) 2019 Daichi GOTO
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are 
+ * met: 
+ * 
+ * 1. Redistributions of source code must retain the above copyright 
+ *    notice, this list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in the 
+ *    documentation and/or other materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "command.h"
+
+struct textset cmdtextsets[] = {
+	{ "command_version", "en_", VERSION },
+
+	{ "command_name", "en_", CMDNAME },
+
+	{ "command_alias", "en_", ALIAS },
+
+	{ "command_copyright", "en_", "2019 ONGS Inc." },
+
+	{ "command_comment", "ja_JP",
+	  "SSVデータを有効SSVデータへ変換して出力" },
+
+	{ "command_comment", "en_", 
+	  "convert the SSV data to the Yuko SSV data" },
+
+	{ "command_synopsis", "en_", 
+	  _CMD(CMDNAME) " "
+	  "[" _OPT("d") "] "
+	  "[" _OPT("hvD") "] [" _OPT("-") "] "
+	  "[" _ARG("file") " " _ETC "]" },
+
+	{ "command_description", "ja_JP", 
+	  "SSVデータを有効SSVデータへ変換して出力。ファイルの指定が\n"
+	  "ないか、-が指定されている場合には標準入力を使用。\n" 
+	  "\n"
+	  "処理できるSSVデータはUTF-8で次の形式である必要がある。\n"
+	  "\n"
+	  "	ID タイムスタンプ 任意 有効 任意 任意 任意 ...\n"
+	  "\n"
+	  "または\n"
+	  "\n"
+	  "	ID タイムスタンプ 任意 無効 任意 任意 任意 ...\n"
+	  "\n"
+	  "たとえば、次のようなデータが処理対象となる。\n"
+	  "\n"
+	  "	0000684184 20190809102300 @ 有効 12 @\n"
+	  "	0000684184 20190809102309 @ 無効 0 @\n"
+	  "	0000684179 20190809102333 @ 無効 0 @\n"
+	  "	0000684184 20190809102341 @ 有効 31 @\n"
+	  "	0000684184 20190809102610 @ 無効 0 有\n"
+	  "	0000684179 20190809102629 @ 有効 7 有\n"
+	  "	0000684179 20190809102323 @ 無効 0 @\n"
+	  "	0000684190 20190809102633 @ 無効 0 有\n"
+	  "	0000684190 20190809102629 @ 有効 56 有\n"
+	  "	0000684192 20190809102634 @ 有効 55 有\n"
+	  "\n"
+	  "上記データであれば、1列目の値ごとにもっとも新しく、かつ、\n"
+	  "4列目が\"有効\"であるデータが抽出される。\n"
+	  "\n"
+	  "	0000684179 20190809102629 @ 有効 7 有\n"
+	  "	0000684192 20190809102634 @ 有効 55 有"
+	},
+
+	{ "command_description", "en_", 
+	  "Convert the SSV data to the Yuko SSV data. If " 
+	  _ARG("file")" is a\nsingle dash (`-') or absent, it reads "
+	  "from the standard input.\n" 
+	  "\n"
+	  "The SSV data that can be processed must be in UTF-8 and in "
+	  "the \nfollowing format.\n"
+	  "\n"
+	  "	ID timestamp any 有効 any any any ...\n"
+	  "\n"
+	  "または\n"
+	  "\n"
+	  "	ID timestamp any 無効 any any any ...\n"
+	  "\n"
+	  "For example, the following data can be processed.\n"
+	  "\n"
+	  "	0000684184 20190809102300 @ 有効 12 @\n"
+	  "	0000684184 20190809102309 @ 無効 0 @\n"
+	  "	0000684179 20190809102333 @ 無効 0 @\n"
+	  "	0000684184 20190809102341 @ 有効 31 @\n"
+	  "	0000684184 20190809102610 @ 無効 0 T\n"
+	  "	0000684179 20190809102629 @ 有効 7 T\n"
+	  "	0000684179 20190809102323 @ 無効 0 @\n"
+	  "	0000684190 20190809102633 @ 無効 0 T\n"
+	  "	0000684190 20190809102629 @ 有効 56 T\n"
+	  "	0000684192 20190809102634 @ 有効 55 T\n"
+	  "\n"
+	  "For the above data, the latest data is extracted for each "
+	  "ID\n""in the 1st column, and the data in the 4th column is "
+	  "\"有効\".\n"
+	  "\n"
+	  "	0000684179 20190809102629 @ 有効 7 T\n"
+	  "	0000684192 20190809102634 @ 有効 55 T"
+	},
+
+	{ "command_options", "ja_JP", 
+	  _OPT("d") "		2列目、3列目、4列目を出力しない\n"
+	  _OPT("h") "		使い方表示\n"
+	  _OPT("v") "		バージョン表示\n"
+	  _OPT("D") "		デバッグモード\n"
+	  _OPT("-") "		オプションの終了を指定\n"
+	  _ARG("file")"\t	ファイルを指定" },
+	
+	{ "command_options", "en_", 
+	  _OPT("d") "		Does not output the 2nd, 3rd, and 4th "
+	  			"columns.\n"
+	  _OPT("h") "		Print the usage message.\n"
+	  _OPT("v") "		Print the version.\n"
+	  _OPT("D") "		Enable the debug mode.\n"
+	  _OPT("-") "		Specify the end of options.\n"
+	  _ARG("file")"\t	Specify the file." },
+
+	{ "command_example", "en_", 
+	  _P("cat data.ssv")
+	  _S("0000684184 20190809102300 @ 有効 12 @")
+	  _S("0000684184 20190809102309 @ 無効 0 @")
+	  _S("0000684179 20190809102333 @ 無効 0 @")
+	  _S("0000684184 20190809102341 @ 有効 31 @")
+	  _S("0000684184 20190809102610 @ 無効 0 有")
+	  _S("0000684179 20190809102629 @ 有効 7 有")
+	  _S("0000684179 20190809102323 @ 無効 0 @")
+	  _S("0000684190 20190809102633 @ 無効 0 有")
+	  _S("0000684190 20190809102629 @ 有効 56 有")
+	  _S("0000684192 20190809102634 @ 有効 55 有")
+	  _P("conv_ssv2yukossv data.ssv")
+	  _S("0000684179 20190809102629 @ 有効 7 有")
+	  _S("0000684192 20190809102634 @ 有効 55 有")
+	  _P("conv_ssv2yukossv -d data.ssv")
+	  _S("0000684179 7 有")
+	  _S("0000684192 55 有")
+	  _P("") },
+
+	TEXTSET_END
+};
